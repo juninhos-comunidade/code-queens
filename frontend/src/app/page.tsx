b911280ce4ba777476b825f82424d0e9d93865e5
Kelly C Santos <kellycristinasntos@gmail.com>
2026-07-25 21:07:27 -0300

    feat: ajustando home para testes

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,10 +1,18 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import { Select } from "@/components";
 
 export default function Home() {
+  const stateOptions = [
+    { value: "sp", label: "São Paulo" },
+    { value: "rj", label: "Rio de Janeiro" },
+    { value: "bh", label: "Belo Horizonte" }
+  ];
   return (
-    <div className={styles.page}>
+    <main className={styles.page}>
      <h1>Stack Check!</h1>
-    </div>
+     <Select label="Estado:" isInvalid errorMessage="Estado inválido" labelFor="estado" options={stateOptions} />
+     <Select label="Cidade:" labelFor="cidade" options={stateOptions} />
+    </main>
   );
 }
