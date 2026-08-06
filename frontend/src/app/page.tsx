e95fab6149f4aa9d8c01edde443e1e49001f6fcd
Kelly C Santos <kellycristinasntos@gmail.com>
2026-08-06 01:18:40 -0300

    fix: ajuste layout

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -3,18 +3,18 @@ import styles from "./page.module.css";
 import { Footer, Navbar } from "@/components";
 
 export default function Home() {
-      const links = [
-        { title: 'sobre', links: [{ title: 'Termos de uso', url: '/termos' }, { title: 'Direitos autorais', url: '/direitos' },{ title: 'Termos de Responsabilidade', url: '/termos' }, { title: 'Direitos pessoais', url: '/direitos' }] },
-        { title: 'suporte', links: [{ title: 'Central de ajuda', url: '/central-ajuda' }] },
-        { title: 'Mapa do site', links: [{ title: 'Inicio', url: '/' }, { title: 'Login', url: '/sign-in' }, { title: 'Cadastro', url: '/sign-up' }] }
-    ]
+  const links = [
+    { title: 'sobre', links: [{ title: 'Termos de uso', url: '/termos' }, { title: 'Direitos autorais', url: '/direitos' }, { title: 'Termos de Responsabilidade', url: '/termos' }, { title: 'Direitos pessoais', url: '/direitos' }] },
+    { title: 'suporte', links: [{ title: 'Central de ajuda', url: '/central-ajuda' }] },
+    { title: 'Mapa do site', links: [{ title: 'Inicio', url: '/' }, { title: 'Login', url: '/sign-in' }, { title: 'Cadastro', url: '/sign-up' }] }
+  ]
   return (
-    <>
-    <Navbar/>
-    <main className={styles.page}>
-    
+    <div className={styles.page}>
+      <Navbar />
+      <main className={styles.page_main}>
 
-    {/* <h3>Stack Check!</h3>
+
+        {/* <h3>Stack Check!</h3>
       <SignCard>
         <Input label="E-mail" labelFor="email" placeholder="Digite seu email" type="email" />
         <Input label="Senha" labelFor="password" placeholder="Digite sua senha" type="password" />
@@ -31,9 +31,9 @@ export default function Home() {
           <a href="">Crie sua conta</a>
         </span>
       </SignCard> */}
-      
-    </main>
-      <Footer linksFooter={links}/>
-    </>
+
+      </main>
+      <Footer linksFooter={links} />
+    </div>
   );
 }
