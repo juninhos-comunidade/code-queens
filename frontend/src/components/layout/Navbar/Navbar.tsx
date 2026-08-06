4b10011c56d0491673d3d6a19a1113a466f376fd
Kelly C Santos <kellycristinasntos@gmail.com>
2026-08-06 00:32:11 -0300

    fix: fechando menu ao clicar fora

--- frontend/src/components/layout/Navbar/Navbar.tsx
+++ frontend/src/components/layout/Navbar/Navbar.tsx
@@ -1,5 +1,5 @@
 'use client';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import Image from 'next/image';
 
 import { MenuIcon, X } from 'lucide-react';
@@ -11,25 +11,35 @@ import styles from './navbar.module.scss'
 
 export const Navbar = () => {
 
-    const [isOpen,setIsOpen] = useState<boolean>(false)
+    const [isOpen, setIsOpen] = useState<boolean>(false)
     const handleToggleNavbar = () => setIsOpen(!isOpen)
-    const visibleIcon = isOpen ? <X/> : <MenuIcon/>
+    const visibleIcon = isOpen ? <X /> : <MenuIcon />
 
+
+
+    useEffect(() => {
+        const menu = document.getElementById('navbar_container');
+        document.addEventListener('pointerdown', () => {
+            const target = event?.target as Node;
+            isOpen && !menu?.contains(target) && setIsOpen(false)
+        })
+      
+    });
     return (
         <nav className={styles.navbar}>
             <Image src={StackCheckLogo} alt='Stack Check' />
 
-            <ul className={`${styles.navbar_container} ${isOpen && styles.navbar_open}`}>
-                <li><a href=''>Item</a></li>
-                <li><a href=''>Item</a></li>
-                <li><a href=''>Item</a></li>
-                <li><a href=''>Item</a></li>
-                <li><a href=''>Item</a></li>
+            <ul
+                id='navbar_container'
+                onClick={() => setIsOpen(false)}
+                className={`${styles.navbar_container} ${isOpen && styles.navbar_open}`}>
+                <li><a href='/'>Home</a></li>
+
             </ul>
 
             <div className={styles.buttons_container}>
                 <button onClick={handleToggleNavbar}>{visibleIcon}</button>
-                <Button text='Login' variant='secondary' minSize={true} className={styles.loginButton}/>
+                <Button text='Login' variant='secondary' minSize={true} className={styles.loginButton} />
             </div>
         </nav>
     )
