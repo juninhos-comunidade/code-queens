39f18935a289a734070ad2eb28d89241d66fff63
Kelly C Santos <kellycristinasntos@gmail.com>
2026-08-05 23:53:54 -0300

    feat: add navbar mobile e minsize ao button

--- frontend/src/components/layout/Navbar/Navbar.tsx
+++ frontend/src/components/layout/Navbar/Navbar.tsx
@@ -1,22 +1,36 @@
+'use client';
+import { useState } from 'react';
 import Image from 'next/image';
 
-import { MenuIcon } from 'lucide-react';
+import { MenuIcon, X } from 'lucide-react';
 import StackCheckLogo from '../../../../public/stackcheck_logo.svg';
+
+import { Button } from '@/components';
 
 import styles from './navbar.module.scss'
 
 export const Navbar = () => {
-    return(
-        <nav className={styles.navbar}>
-            <Image src={StackCheckLogo} alt='Stack Check'/>
 
-            <ul className={styles.navbar_container}>
+    const [isOpen,setIsOpen] = useState<boolean>(false)
+    const handleToggleNavbar = () => setIsOpen(!isOpen)
+    const visibleIcon = isOpen ? <X/> : <MenuIcon/>
+
+    return (
+        <nav className={styles.navbar}>
+            <Image src={StackCheckLogo} alt='Stack Check' />
+
+            <ul className={`${styles.navbar_container} ${isOpen && styles.navbar_open}`}>
+                <li><a href=''>Item</a></li>
+                <li><a href=''>Item</a></li>
+                <li><a href=''>Item</a></li>
+                <li><a href=''>Item</a></li>
                 <li><a href=''>Item</a></li>
             </ul>
 
-            <button> <MenuIcon/></button>
-            <button>Login</button>
-            {/* <Button text='Login' /> */}
+            <div className={styles.buttons_container}>
+                <button onClick={handleToggleNavbar}>{visibleIcon}</button>
+                <Button text='Login' variant='secondary' minSize={true} className={styles.loginButton}/>
+            </div>
         </nav>
     )
 }
