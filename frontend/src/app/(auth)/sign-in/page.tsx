4a9cb771d7b6f3e937c7e02b8e37014dfe34e39c
Kelly C Santos <kellycristinasntos@gmail.com>
2026-08-08 15:40:06 -0300

    fix: importando compoente authcontainer

--- frontend/src/app/(auth)/sign-in/page.tsx
+++ frontend/src/app/(auth)/sign-in/page.tsx
@@ -2,7 +2,7 @@
 import { zodResolver } from "@hookform/resolvers/zod";
 import { loginSchema, LoginFormData } from "./useAuth";
 
-import { SignCard, Input, Checkbox, Button } from "@/components";
+import { SignCard, Input, Checkbox, Button, AuthContainer } from "@/components";
 import { useAuth } from "./useAuth";
 
 import styles from './page.module.scss'
@@ -19,45 +19,55 @@ const SignIn = () => {
         console.log("Valid form data payload:", data);
     }
     return (
-        <div className={styles.page}>
-            <div className={styles.introdution_section}>
-                <h4>Entrar na sua conta</h4>
-                <p>Acesse sua conta para continuar aprendendo e evoluindo</p>
-            </div>
-            <SignCard>
+        <AuthContainer
+            title="*Entrar na sua conta*"
+            subtitle="Acesse sua conta para continuar aprendendo e evoluindo"
+            optionalChildren={
+                <>
+                    Ainda não tem uma conta?
+                    <a href="/sign-up">Crie sua conta</a>
+                </>
+            }
+        />
+        // <div className={styles.page}>
+        //     <div className={styles.introdution_section}>
+        //         <h4>Entrar na sua conta</h4>
+        //         <p>Acesse sua conta para continuar aprendendo e evoluindo</p>
+        //     </div>
+        //     <SignCard>
 
-                <form onSubmit={handleSubmit(onSubmit)}>
-                    <Input
-                        label="E-mail"
-                        labelFor="email"
-                        placeholder="Digite seu email"
-                        type="email"
-                        {...register("email")}
-                    />
-                    <Input
-                        label="Senha"
-                        labelFor="password"
-                        placeholder="Digite sua senha"
-                        type="password"
-                        msgError={errors?.password?.message}
-                        {...register("password")}
-                    />
-                    <div className={styles.passwordHelper}>
-                        <Checkbox label="Lembrar de mim" value="lembrar_de_mim" />
-                        <a href="/">Esqueci minha senha</a>
-                    </div>
-                    <Button type="submit" text="Entrar" disabled={isSubmitting}/>
+        //         <form onSubmit={handleSubmit(onSubmit)}>
+        //             <Input
+        //                 label="E-mail"
+        //                 labelFor="email"
+        //                 placeholder="Digite seu email"
+        //                 type="email"
+        //                 {...register("email")}
+        //             />
+        //             <Input
+        //                 label="Senha"
+        //                 labelFor="password"
+        //                 placeholder="Digite sua senha"
+        //                 type="password"
+        //                 msgError={errors?.password?.message}
+        //                 {...register("password")}
+        //             />
+        //             <div className={styles.passwordHelper}>
+        //                 <Checkbox label="Lembrar de mim" value="lembrar_de_mim" />
+        //                 <a href="/">Esqueci minha senha</a>
+        //             </div>
+        //             <Button type="submit" text="Entrar" disabled={isSubmitting}/>
 
-                    <div className={styles.divisor}>
-                        <hr /><p>ou</p> <hr />
-                    </div>
-                    <span className={styles.optinalContainer}>
-                        Ainda não tem uma conta?
-                        <a href="/sign-up">Crie sua conta</a>
-                    </span>
-                </form>
-            </SignCard>
-        </div>
+        //             <div className={styles.divisor}>
+        //                 <hr /><p>ou</p> <hr />
+        //             </div>
+        //             <span className={styles.optinalContainer}>
+        //                 Ainda não tem uma conta?
+        //                 <a href="/sign-up">Crie sua conta</a>
+        //             </span>
+        //         </form>
+        //     </SignCard>
+        // </div>
     )
 }
 export default SignIn;
