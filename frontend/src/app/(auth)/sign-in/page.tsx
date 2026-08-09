99c0e6f29f28a5e6ba0fbb4493ef3fe615a21a50
Kelly C Santos <kellycristinasntos@gmail.com>
2026-08-09 03:15:12 -0300

    fix: utilizando componente authcontainer

--- frontend/src/app/(auth)/sign-in/page.tsx
+++ frontend/src/app/(auth)/sign-in/page.tsx
@@ -2,13 +2,8 @@
 import { zodResolver } from "@hookform/resolvers/zod";
 import { loginSchema, LoginFormData } from "./useAuth";
 
-import { SignCard, Input, Checkbox, Button, AuthContainer } from "@/components";
-import { useAuth } from "./useAuth";
-
-import styles from './page.module.scss'
+import { Input, Checkbox, AuthContainer } from "@/components";
 import { useForm } from "react-hook-form";
-
-
 
 const SignIn = () => {
     const { register, handleSubmit, formState: { errors, isSubmitting }, } = useForm<LoginFormData>({
@@ -20,54 +15,42 @@ const SignIn = () => {
     }
     return (
         <AuthContainer
-            title="*Entrar na sua conta*"
-            subtitle="Acesse sua conta para continuar aprendendo e evoluindo"
+            submit={handleSubmit(onSubmit)}
+            isLoading={isSubmitting}
+            title="Acesse sua conta"
+            subtitle="Acesse sua conta para continuar aprendendo e evoluindo."
+            formChildren={
+                <>
+                    <Input
+                        label="E-mail"
+                        labelFor="email"
+                        placeholder="Digite seu email"
+                        type="email"
+                        {...register("email")}
+                    />
+                    <Input
+                        label="Senha"
+                        labelFor="password"
+                        placeholder="Digite sua senha"
+                        type="password"
+                        msgError={errors?.password?.message}
+                        {...register("password")}
+                    />
+                </>
+            }
             optionalChildren={
                 <>
                     Ainda não tem uma conta?
                     <a href="/sign-up">Crie sua conta</a>
                 </>
             }
+            helperChildren={
+                <>
+                    <Checkbox label="Lembrar de mim" value="lembrar_de_mim" />
+                    <a href="/">Esqueci minha senha</a>
+                </>
+            }
         />
-        // <div className={styles.page}>
-        //     <div className={styles.introdution_section}>
-        //         <h4>Entrar na sua conta</h4>
-        //         <p>Acesse sua conta para continuar aprendendo e evoluindo</p>
-        //     </div>
-        //     <SignCard>
-
-        //         <form onSubmit={handleSubmit(onSubmit)}>
-        //             <Input
-        //                 label="E-mail"
-        //                 labelFor="email"
-        //                 placeholder="Digite seu email"
-        //                 type="email"
-        //                 {...register("email")}
-        //             />
-        //             <Input
-        //                 label="Senha"
-        //                 labelFor="password"
-        //                 placeholder="Digite sua senha"
-        //                 type="password"
-        //                 msgError={errors?.password?.message}
-        //                 {...register("password")}
-        //             />
-        //             <div className={styles.passwordHelper}>
-        //                 <Checkbox label="Lembrar de mim" value="lembrar_de_mim" />
-        //                 <a href="/">Esqueci minha senha</a>
-        //             </div>
-        //             <Button type="submit" text="Entrar" disabled={isSubmitting}/>
-
-        //             <div className={styles.divisor}>
-        //                 <hr /><p>ou</p> <hr />
-        //             </div>
-        //             <span className={styles.optinalContainer}>
-        //                 Ainda não tem uma conta?
-        //                 <a href="/sign-up">Crie sua conta</a>
-        //             </span>
-        //         </form>
-        //     </SignCard>
-        // </div>
     )
 }
 export default SignIn;
