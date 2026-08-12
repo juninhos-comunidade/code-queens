b8d1aeb409c33ff428cd4a508892aa55b74b36fa
Kelly C Santos <kellycristinasntos@gmail.com>
2026-08-12 18:42:35 -0300

    feat: add empty state

--- frontend/src/app/(dashboard)/dashboard/page.tsx
+++ frontend/src/app/(dashboard)/dashboard/page.tsx
@@ -5,12 +5,12 @@ import styles from './dashboard.module.scss'
 import { Button, Limit, StackTag, Tag } from "@/components";
 import { CloudLightning, Play, TurkishLiraIcon } from "lucide-react";
 import { HistoryCard } from "@/components/feature/HistoryCard/HistoryCard";
+import EmptyTest from '../../../../public/empty-test-icon.svg';
+
 const Dashboard = () => {
 
   const { user, full_name, nickname } = useDashboard()
-  const stacks = ['css', 'html', 'vitest', 'javascript', 'vue', 'node']
-
-  const history = [
+  const history =  [
     {
       index: 1,
       titleTest: 'Teste HTML e CSS',
@@ -40,7 +40,7 @@ const Dashboard = () => {
       <Limit>
         <div className={styles.header_container}>
           <div className={styles.welcome}>
-            <h2>Olá, <span className={styles.username}>{full_name || 'Fanny'}</span>! Vamos nessa?</h2>
+            <h3>Olá, <span className={styles.username}>{full_name || 'Fanny'}</span>! Vamos nessa?</h3>
             <p>Acompanhe seus testes realizados, veja detalhes e continue evoluindo.</p>
             <Button text="Iniciar novo teste" icon={<Play />} />
           </div>
@@ -48,20 +48,32 @@ const Dashboard = () => {
         </div>
 
         <div className={styles.table_container}>
-          <h4>
-            Seus testes realizados
-          </h4>
           {
-            history.map((item) => (
+            history.length ?
+              <>
+                <h4>
+                  Seus testes realizados
+                </h4>
+                {
+                  history?.map(item => (
 
-              <HistoryCard
-                index={item.index}
-                titleTest={item.titleTest}
-                date={item.date}
-                stacks={item.stacks}
-                progress={item.progress}
-              />
-            ))
+                    <HistoryCard
+                      index={item.index}
+                      titleTest={item.titleTest}
+                      date={item.date}
+                      stacks={item.stacks}
+                      progress={item.progress}
+                    />
+                  ))
+                }
+              </>
+              :
+              <section className={styles.empty}>
+                <Image src={EmptyTest} alt="teste vazio" />
+                <h4>Você ainda não realizou nenhum teste</h4>
+                <p>Escolha uma tecnologia, inicie seu primeiro teste e descubra seu nível de proficiência técnica de forma rápida e prática!</p>
+                <Button text="Iniciar meu primeiro teste" />
+              </section>
           }
         </div>
       </Limit>
