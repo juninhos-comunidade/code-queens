67dc4c0aaa218bb8990d68cf964f0ad858ef1f78
Kelly C Santos <kellycristinasntos@gmail.com>
2026-08-14 02:41:13 -0300

    fix: ajuste no formato da data de aniversario

--- frontend/src/app/(auth)/sign-up/useSignUp.ts
+++ frontend/src/app/(auth)/sign-up/useSignUp.ts
@@ -7,14 +7,14 @@ import { useRouter } from 'next/navigation';
 export const subscribeSchema = z.object({
     full_name: z.string().min(6, 'Informe como devemos te chamar'),
     email: z.email({ message: 'Você deve informar um e-mail valido' }),
-    birth_date: z.iso.date({ message: 'Informe quando nasceu' }),
+    birth_date: z.string({ message: 'Informe quando nasceu' }).transform((date) => (date).split('/').reverse().join().replaceAll(',','-')),
     uf: z.string().min(1, 'Informe o estado onde mora'),
     gender: z.string().min(1, 'Informe o genero com o qual se identifica'),
     password: z.string().min(6, 'Sua senha deve conter no minimo 6 caracteres'),
     confirmPassword: z.string({ message: 'Você deve confirmar sua senha' }),
     id_security_questions: z.number().or(z.string().min(1, 'Deu ruim')),
     answer_security_question: z.string().trim().min(3, 'Sua resposta deve ter no minimo 3 caracteres'),
-    termsAcepted: z.boolean().refine(Boolean, {
+    accepted: z.boolean().refine(Boolean, {
         message: "Você deve aceitar os termos e políticas da plataforma",
     }),
 }).superRefine((data, ctx) => {
