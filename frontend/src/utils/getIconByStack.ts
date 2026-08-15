c9f4414b8320d3357f383aba27e30a025d508e7e
Kelly C Santos <kellycristinasntos@gmail.com>
2026-08-15 19:43:26 -0300

    fix: ajustando get icon

--- frontend/src/utils/getIconByStack.ts
+++ frontend/src/utils/getIconByStack.ts
@@ -2,6 +2,6 @@ import { stackIcon } from "@/constants"
 import { Stack } from "@/types"
 
 export const getIconByStack = (stack: Stack) => {
-    console.log(stackIcon[stack.toLowerCase() as Stack])
-  return stackIcon[stack.toLowerCase() as Stack].src
+    console.log('icone',stackIcon[stack.toLowerCase() as Stack])
+  return stackIcon[stack.toLowerCase().replaceAll('.','') as Stack].src
 } 
