d8a7f5b8d4ebfb719126a72b0e73b5a9412d8b55
Kelly C Santos <kellycristinasntos@gmail.com>
2026-08-15 06:25:06 -0300

    feat: trazendo dados do historico de teste

--- frontend/src/app/(dashboard)/dashboard/useDashboard.ts
+++ frontend/src/app/(dashboard)/dashboard/useDashboard.ts
@@ -1,14 +1,32 @@
+'use client'
 import { useUserStore } from "@/store"
 import { useRouter } from 'next/navigation';
 
+import { getAssessmentsHistory } from "@/api/endpoints";
+import { useState } from "react";
 
 export const useDashboard = () => {
 
-    const {user} = useUserStore((state) => state)
+    const {user} = useUserStore()
+    const {id_users} = user
+
     const router = useRouter()
     const navigationToTest = () => router.push('/test')
     const {full_name} = user
+    const [history, setHistory] = useState<AssessmentsHistory[] | []>([])
 
-    return{user,full_name, navigationToTest}
+    const getAssessmentsHistoryById = async () => {
+        id_users &&
+        await getAssessmentsHistory(id_users)
+        .then((response) => {
+            setHistory(response)
+            console.log()
+        }
+        )
+        .catch((error) => console.error(error))
+        .finally()
+    }
+
+    return{user,id_users, full_name, history, navigationToTest,getAssessmentsHistoryById}
 
 }
