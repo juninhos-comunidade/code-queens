0630b3c359cf165f474eedfeabf17f3ac1f83276
Kelly C Santos <kellycristinasntos@gmail.com>
2026-08-16 03:58:25 -0300

    fix? ajustando tipagem dos termos

--- frontend/src/app/(terms)/privacy-policy/page.tsx
+++ frontend/src/app/(terms)/privacy-policy/page.tsx
@@ -1,30 +1,31 @@
 'use client'
 import { useState, useEffect } from "react";
 import { getPrivacyPolicy } from "@/api/endpoints";
+import { Term } from "@/types";
 import { Limit } from "@/components";
 
-
 const PoliticaPrivacidade = () => {
-    const [useTerm, setUseTerm] = useState<string>('')
-    
+    const [useTerm, setUseTerm] = useState<Term>()
 
-    useEffect( () => {
-     getPrivacyPolicy()
-     .then(response => {
+    useEffect(() => {
+        getPrivacyPolicy()
+            .then(response => {
 
-         setUseTerm(response)
-         console.log(useTerm?.term_description)
-     }
-    )
-     .catch((error) => console.error('Tivemos um erro ao recuperar as politicas de privacidade', error))
+                setUseTerm(response)
+                console.log(useTerm?.term_description)
+            }
+            )
+            .catch((error) => console.error('Tivemos um erro ao recuperar as politicas de privacidade', error))
     }, [])
 
     return (
         <Limit>
-            <div className="term_container" 
-            dangerouslySetInnerHTML={{__html: useTerm && useTerm?.term_description}}>
-
-            </div>
+            {
+                useTerm &&
+                <div className="term_container"
+                    dangerouslySetInnerHTML={{ __html: useTerm && useTerm?.term_description }}>
+                </div>
+            }
         </Limit>
     )
 }
--- frontend/src/app/(terms)/terms-of-use/page.tsx
+++ frontend/src/app/(terms)/terms-of-use/page.tsx
@@ -1,30 +1,32 @@
 'use client'
 import { useState, useEffect } from "react";
 import { getUseTerm } from "@/api/endpoints";
+import { Term } from "@/types";
 import { Limit } from "@/components";
 
-
 const TermoDeUso = () => {
-    const [useTerm, setUseTerm] = useState<string>('')
-    
+    const [useTerm, setUseTerm] = useState<Term>()
 
-    useEffect( () => {
-     getUseTerm()
-     .then(response => {
 
-         setUseTerm(response)
-         console.log(useTerm?.term_description)
-     }
-    )
-     .catch((error) => console.error('Tivemos um erro ao recuperar as politicas de privacidade', error))
+    useEffect(() => {
+        getUseTerm()
+            .then(response => {
+
+                setUseTerm(response)
+                console.log(useTerm?.term_description)
+            }
+            )
+            .catch((error) => console.error('Tivemos um erro ao recuperar as politicas de privacidade', error))
     }, [])
 
     return (
         <Limit>
-            <div className="term_container" 
-            dangerouslySetInnerHTML={{__html: useTerm && useTerm?.term_description}}>
-
-            </div>
+            {
+                useTerm &&
+                <div className="term_container"
+                    dangerouslySetInnerHTML={{ __html: useTerm?.term_description }}>
+                </div>
+            }
         </Limit>
     )
 }
