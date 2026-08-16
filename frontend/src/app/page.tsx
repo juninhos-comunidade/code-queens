337cf80e345fb9349fc7f9c95ac5123c87d0b024
Kelly C Santos <kellycristinasntos@gmail.com>
2026-08-16 19:45:11 -0300

    fix: ajuste de textos e card

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -6,7 +6,7 @@ import { useRouter } from "next/navigation";
 
 
 export default function Home() {
-const router = useRouter()
+  const router = useRouter()
   return (
     <div className={styles.page}>
       <Navbar />
@@ -17,16 +17,17 @@ const router = useRouter()
               <h1>
                 Coloque suas <span className='emphasys'>stacks</span> em <span className='emphasys'>check</span> e avance de verdade
               </h1>
-              <p>Resolva testes com problemas reais do dia a dia de um desenvolvedor e saia na frente em sua próxima entrevista.</p>
+              <p>Descubra se você está preparado para as vagas que deseja conquistar. Teste suas habilidades, 
+                identifique seus pontos de melhoria e evolua com confiança para sua próxima entrevista.</p>
               <div className={styles.button_container}>
-                <Button text="Cadastrar agora" onClick={() => router.push('/sign-up')}/>
-                <Button text="Login" variant="outline"  onClick={() => router.push('/sign-in')} />
+                <Button text="Começar agora" onClick={() => router.push('/sign-up')} />
+                <Button text="Já tenho uma conta" variant="outline" onClick={() => router.push('/sign-in')} />
               </div>
             </div>
 
-            <div className={styles.header_image}>
+            {/* <div className={styles.header_image}>
 
-            </div>
+            </div> */}
           </div>
 
         </Limit>
@@ -38,13 +39,14 @@ const router = useRouter()
             <div className={styles.proposal_image}></div>
             <div className={styles.proposal_info}>
               <h2>O que fazemos?</h2>
-              <p>O StackCheck cria testes técnicos personalizados para desenvolvedores que buscam sair na frente
-                em suas entrevistas.Geramos automaticamente um teste baseado no seu objetivo e você pode definir:
+              <p>O StackCheck cria avaliações técnicas personalizadas para ajudar desenvolvedores a entender seu nível atual, 
+                identificar oportunidades de evolução e chegar mais preparado aos desafios do mercado.
+
               </p>
               <ul>
-                <li><CircleCheck /> Frente de atuação</li>
-                <li><CircleCheck /> Senioridade desejada</li>
-                <li><CircleCheck /> E as tecnologias que quer avaliar</li>
+                <li><CircleCheck /> Área de atuação</li>
+                <li><CircleCheck />  Nível desejado</li>
+                <li><CircleCheck /> Tecnologias que deseja praticar</li>
               </ul>
             </div>
           </Limit>
@@ -53,8 +55,8 @@ const router = useRouter()
         <section className={styles.features_section}>
           <Limit>
             <div className={styles.feature_title}>
-              <h2>Iremos <span className='emphasys'>destravar</span> o seu próximo <span className='emphasys'>“sim”</span> </h2>
-              <p>Escolha sua área, monte seu teste e descubra exatamente o que falta para chegar mais confiante na próxima entrevista.</p>
+              <h2> O próximo <span className='emphasys'>sim</span>{' '}da sua carreira pode estar a poucos testes de distância</h2>
+              <p> Monte um teste alinhado aos seus objetivos e descubra os conhecimentos que podem acelerar sua evolução profissional.</p>
             </div>
 
             {/* Cards */}
@@ -63,32 +65,33 @@ const router = useRouter()
 
                 <div className={styles.feature_card}>
                   <h3>Teste suas  <span className='emphasys'>habilidades</span></h3>
-                  <p>Responda a testes personalizados focado em entrevistas reais e descubra seu nível em cada stack.</p>
+                  <p>Resolva desafios inspirados em situações reais do mercado e descubra exatamente quais conhecimentos já são seus pontos fortes e quais ainda podem evoluir.
+</p>
                   <ul>
                     <li>
                       <CircleCheck /> Até 25 perguntas por teste </li>
-                    <li><CircleCheck />Baseado nas stacks escolhidas </li>
+                    <li><CircleCheck />Stacks que você escolhe </li>
                     <li><CircleCheck /> Adaptados à senioridade selecionada </li>
                   </ul>
                 </div>
                 <div className={styles.feature_card}>
-                  <h3>Receba   <span className="emphasys">feedbacks</span></h3>
-                  <p>Após concluir, receba uma análise detalhada do seu desempenho e descubra exatamente onde focar.</p>
+                  <h3>Receba<span className="emphasys">{' '}feedbacks</span></h3>
+                  <p>Saia do teste sabendo exatamente o próximo passo</p>
                   <ul>
                     <li>
-                      <CircleCheck /> Nota geral imediata </li>
-                    <li><CircleCheck />Análise por tecnologia </li>
-                    <li><CircleCheck /> Dicas de estudo</li>
+                      <CircleCheck /> Resultado imediato </li>
+                    <li><CircleCheck /> Feedback por tecnologia </li>
+                    <li><CircleCheck />  Recomendações personalizadas</li>
                   </ul>
                 </div>
                 <div className={styles.feature_card}>
-                  <h3>Acompanhe <span className="emphasys">evolução</span></h3>
+                  <h3>Acompanhe a<span className="emphasys">evolução</span></h3>
                   <p>Veja seus resultados anteriores e acompanhe seu crescimento profissional ao longo do tempo.</p>
                   <ul>
                     <li>
                       <CircleCheck /> Histórico completo </li>
-                    <li><CircleCheck />Gráfico de progresso </li>
-                    <li><CircleCheck /> Gráfico de progresso</li>
+                    <li><CircleCheck />Evolução por tecnologia </li>
+                    <li><CircleCheck />Progresso ao longo do tempo</li>
                   </ul>
                 </div>
               </div>
