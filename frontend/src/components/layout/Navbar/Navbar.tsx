b11be3bb863536a7b266e22f30f83abe2f8948c9
Kelly C Santos <kellycristinasntos@gmail.com>
2026-08-16 00:20:29 -0300

    feat: exibindo menu de acordo estar logado ou nao

--- frontend/src/components/layout/Navbar/Navbar.tsx
+++ frontend/src/components/layout/Navbar/Navbar.tsx
@@ -14,25 +14,33 @@ import Link from 'next/link';
 export const Navbar = () => {
     const router = useRouter()
     const [isOpen, setIsOpen] = useState<boolean>(false)
+    const [token, setToken] = useState<string>()
     const handleToggleNavbar = () => setIsOpen(!isOpen)
     const visibleIcon = isOpen ? <X /> : <MenuIcon />
 
 
 
-useEffect(() => {
-    const handleClickOutside = (event: MouseEvent) => {
-        const target = event.target as Node;
-        const menu = document.getElementById('navbar_container');
+    useEffect(() => {
+        const handleClickOutside = (event: MouseEvent) => {
+            const target = event.target as Node;
+            const menu = document.getElementById('navbar_container');
 
-        if (isOpen && menu && !menu.contains(target)) return setIsOpen(false);
-    };
+            if (isOpen && menu && !menu.contains(target)) return setIsOpen(false);
+        };
 
-    document.addEventListener('pointerdown', handleClickOutside);
+        document.addEventListener('pointerdown', handleClickOutside);
 
-    return () => {
-        document.removeEventListener('pointerdown', handleClickOutside);
-    };
-}, [isOpen]);
+        return () => {
+            document.removeEventListener('pointerdown', handleClickOutside);
+        };
+    }, [isOpen]);
+
+    useEffect(() => {
+        const token = localStorage.getItem("token");
+        token && setToken(token)
+    }, [router]);
+
+
     return (
         <nav className={styles.navbar}>
             <Limit>
@@ -45,15 +53,27 @@ useEffect(() => {
                     onClick={() => setIsOpen(false)}
                     className={`${styles.navbar_container} ${isOpen && styles.navbar_open}`}>
                     <li><Link href='/'>Inicio</Link></li>
-                    <li><Link href='/dashboard'>Dashboard</Link></li>
-                    <li><Link href='/sign-in'>Login</Link></li>
-                    <li><Link href='/sign-up'>Cadastro</Link></li>
+                    {
+                        token ?
+                            <>
+                                <li><Link href='/dashboard'>Dashboard</Link></li>
+                                <li><Link href='/logout'>Logout</Link></li>
+                            </> :
+                            <>
+                                <li><Link href='/sign-in'>Login</Link></li>
+                                <li><Link href='/sign-up'>Cadastro</Link></li>
+                            </>
+                    }
 
                 </ul>
 
                 <div className={styles.buttons_container}>
                     <button className={styles.nav_toggle} onClick={handleToggleNavbar}>{visibleIcon}</button>
-                    <Button onClick={() => router.push('/sign-in')} text='Login' variant='secondary' minSize={true} className={styles.loginButton} />
+                   {
+                   token ?
+                   <Button onClick={() => router.push('/sign-in')} text='Login' variant='secondary' minSize={true} className={styles.loginButton} />
+                :  <Button onClick={() => {}} text='Logout' variant='secondary' minSize={true} className={styles.loginButton} />
+                }
                 </div>
             </Limit>
         </nav>
